30b0a67ee8d37cd9d26699aef83997dbf8877aa2
Muhammad Nasir <nmian7080@gmail.com>
2023-11-04 20:22:18 +0500

    fix  bugs

--- preview.php
+++ preview.php
@@ -1,98 +1,53 @@
-<?php
-$data = file_get_contents('res.json');
-$data = json_decode($data);
-session_start();
-if (isset($_GET['pre'])) {
-  $tem_id = urldecode($_GET['pre']);
-  $_SESSION['my_data'] = $tem_id;
-  header('location: ./select.php');
-}
+const image = document.querySelector(".hotspots-image");
+const canvas = document.getElementById("hover-canvas");
+const ctx = canvas.getContext("2d");
 
+// Assuming all .more-info-area elements are within a parent element with an ID "parent-container"
+const parentContainer = document.getElementById("parent-container");
 
-?>
-<!DOCTYPE html>
-<html lang="en">
+parentContainer.addEventListener("mouseenter", function (event) {
+  if (event.target.classList.contains("more-info-area")) {
+    console.log("function run");
+    const coords = event.target.getAttribute("coords")
+      .split(",")
+      .map((coord) => parseInt(coord));
+    console.log(coords);
+    const x = coords[0];
+    const y = coords[1];
+    const width = coords[4] - coords[0];
+    const height = coords[5] - coords[1];
+    const borderWidth = 2; // Adjust the border width as needed
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Your CV</title>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    canvas.style.top = `${y}px`;
+    canvas.style.left = `${x}px`;
 
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"></script>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-</head>
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-<body>
-  <style>
-    .btns {
-      background: linear-gradient(63deg, #c21010 0%, #a73232 80%);
-      color: white;
-      padding: 8px 18px;
-      border: none;
-      float: right;
-      margin: 10px 40px 30px 0;
-      font-size: 20px;
-      border-radius: 5px;
-      cursor: pointer;
+    // Draw the border
+    ctx.strokeStyle = "rgba(0, 0, 0)";
+    ctx.lineWidth = borderWidth;
+    ctx.strokeRect(
+      borderWidth / 2,
+      borderWidth / 2,
+      width - borderWidth,
+      height - borderWidth
+    );
 
+    // Draw the filled rectangle
+    ctx.fillStyle = "rgba(0, 0, 0, 0.5)";
+    ctx.fillRect(
+      borderWidth,
+      borderWidth,
+      width - 2 * borderWidth,
+      height - 2 * borderWidth
+    );
+
+    canvas.style.display = "block";
+  });
+  
+  parentContainer.addEventListener("mouseout", function (event) {
+    if (event.target.classList.contains("more-info-area")) {
+      canvas.style.display = "none";
     }
-
-    * {
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
-      font-family: Arial, sans-serif;
-    }
-
-    .selecthading h1 {
-      background-color: #C21010;
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      color: white;
-      padding: 18px 32px;
-      border-radius: 10px;
-    }
-  </style>
-
-  <button class="btns" id="downbtn">Download PDF</button>
-
-  <?php
-
-  if (@$_SESSION['my_data']) {
-    include_once('templates/tem_1.php');
-    include_once('templates/tem_2.php');
-  } else {
-  ?>
-    <div class="selecthading">
-      <div class="selecthading">
-        <h1>Please Select Template</h1>
-      </div>
-    <?php
-  }
-    ?>
-    <script>
-      let btn = document.getElementById('downbtn');
-      btn.addEventListener('click', () => {
-        let contents = document.getElementById('content');
-        const opt = {
-          margin: 0,
-          filename: 'Cvbuilder.pdf',
-          html2canvas: {
-            scale: 1,
-          },
-          jsPDF: {
-            unit: 'mm',
-            format: 'a4',
-            orientation: 'portrait'
-          }
-        }
-        html2pdf().set(opt).from(contents).save();
-      });
-    </script>
-
-</body>
-
-</html>
+  });
+});
